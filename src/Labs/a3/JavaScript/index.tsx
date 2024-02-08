ee9dc89e75165cd3c254d1731a6a08f8f04a868d
Serin Serena Jeon <serin0314@gmail.com>
2024-02-08 18:50:22 -0500

    2.3 navigation

--- src/Labs/a3/JavaScript/index.tsx
+++ src/Labs/a3/JavaScript/index.tsx
@@ -4,6 +4,20 @@ import BooleanVariables from "./variables/BooleanVariables";
 import IfElse from "./conditionals/IfElse";
 import TernaryOperator from "./conditionals/TernaryOperator";
 import WorkingWithFunctions from "./functions/WorkingWithFunctions";
+import WorkingWithArrays from "./arrays/WorkingWithArrays";
+import ArrayIndexAndLength from "./arrays/ArrayIndexAndLength";
+import AddingAndRemovingDataToFromArrays from "./arrays/AddingAndRemovingDataToFromArrays";
+import ForLoops from "./arrays/ForLoops";
+import MapFunction from "./arrays/MapFunction";
+import JsonStringify from "./arrays/JsonStringify";
+import FindFunction from "./arrays/FindFunction";
+import FindIndex from "./arrays/FindIndex";
+import FilterFunction from "./arrays/FilterFunction";
+import TemplateLiterals from "./string/TemplateLiterals";
+import House from "./json/House";
+import Spreading from "./json/Spreading";
+import Destructing from "./json/Destructing";
+import FunctionDestructing from "./functions/FunctionDestructing";
 
 function JavaScript() {
     console.log("Hello World!");
@@ -16,6 +30,20 @@ function JavaScript() {
           <IfElse/>
           <TernaryOperator/>
           <WorkingWithFunctions/>
+          <WorkingWithArrays/>
+          <ArrayIndexAndLength/>
+          <AddingAndRemovingDataToFromArrays/>
+          <ForLoops/>
+          <MapFunction/>
+          <JsonStringify/>
+          <FindFunction/>
+          <FindIndex/>
+          <FilterFunction/>
+          <TemplateLiterals/>
+          <House/>
+          <Spreading/>
+          <Destructing/>
+          <FunctionDestructing/>
        </div>
     );
 }
