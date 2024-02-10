fc27cc9b99bef55bc106086cc8ce719f4bc2347b
Serin Serena Jeon <serin0314@gmail.com>
2024-02-10 14:06:15 -0500

    (roughly) till 3.4

---
+++ src/Kanbas/Courses/Navigation/index.tsx
@@ -0,0 +1,19 @@
+import { Link, useLocation } from "react-router-dom";
+import "./index.css"; // feel free to use the CSS from previous assignments
+function CourseNavigation() {
+  const links = ["Home", "Modules", "Piazza", "Assignments", "Quizzes", "Grades",
+                "People", "Panopto Video", "Discussions", "Announcements", "Pages", "Files",
+                "Rubrics", "Outcomes", "Collaborations", "Syllabus", "Settings"];
+  const { pathname } = useLocation();
+  console.log("Pathname in Cou>Nav: " + pathname)
+  return (
+    <ul className="wd-navigation">
+      {links.map((link, index) => (
+        <li key={index} className={pathname.includes(link) ? "wd-active" : ""}>
+          <Link to={`/Kanbas/Courses/${link}`}>{link}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+export default CourseNavigation;
---
+++ src/Kanbas/Courses/index.tsx
@@ -0,0 +1,41 @@
+import { courses } from "../../Kanbas/Database";
+import { useParams, Navigate, Route, Routes } from "react-router-dom";
+import { HiMiniBars3 } from "react-icons/hi2";
+import CourseNavigation from "./Navigation";
+
+function Courses() {
+  const { courseId } = useParams();
+  console.log("CourseID: " + courseId);
+  const course = courses.find((course) => course._id === courseId);
+  console.log("Course: " + course);
+  return (
+    <div>
+      <h1><HiMiniBars3 /> Course {course?.name}</h1>
+      <CourseNavigation />
+      <div>
+        <Routes>
+            <Route path="/" element={<Navigate to="Home" />} />
+            <Route path="Home" element={<h1>Home</h1>} />
+            <Route path="Modules" element={<h1>Modules</h1>} />
+            <Route path="Piazza" element={<h1>Piazza</h1>} />
+            <Route path="Assignments" element={<h1>Assignments</h1>} />
+            <Route path="Assignments/:assignmentId" element={<h1>Assignment Editor</h1>} />
+            <Route path="Quizzes" element={<h1>Quizzes</h1>} />
+            <Route path="Grades" element={<h1>Grades</h1>} />
+            <Route path="People" element={<h1>People</h1>} />
+            <Route path="Panopto Video" element={<h1>Panopto Video</h1>} />
+            <Route path="Discussions" element={<h1>Discussions</h1>} />
+            <Route path="Announcements" element={<h1>Announcements</h1>} />
+            <Route path="Pages" element={<h1>Pages</h1>} />
+            <Route path="Files" element={<h1>Files</h1>} />
+            <Route path="Rubrics" element={<h1>Rubrics</h1>} />
+            <Route path="Outcomes" element={<h1>Outcomes</h1>} />
+            <Route path="Collaborations" element={<h1>Collaborations</h1>} />
+            <Route path="Syllabus" element={<h1>Syllabus</h1>} />
+            <Route path="Settings" element={<h1>Settings</h1>} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+export default Courses;
