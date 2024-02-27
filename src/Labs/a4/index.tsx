3cc1aabd3ca05042994ba9759b27cc32c4a8428e
Serin Serena Jeon <serin0314@gmail.com>
2024-02-27 10:51:14 -0500

    2.3 all done

--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -1,9 +1,35 @@
 import React from "react";
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariables";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
 
 const Assignment4 = () => {
+  function sayHello() {
+    alert("Hello");
+  }
+
   return(
     <>
       <h1>Assignment 4</h1>
+      <ClickEvent />
+      <PassingDataOnEvent/>
+      <PassingFunctions theFunction={sayHello}/>
+      <EventObject/>
+      <Counter/>
+      <BooleanStateVariables/>
+      <StringStateVariables/>
+      <DateStateVariable/>
+      <ObjectStateVariable/>
+      <ArrayStateVariable/>
+      <ParentStateComponent/>
     </>
   );
 };
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,11 +1,17 @@
 import Assignment3 from "./a3";
+import Assignment4 from "./a4";
 import Nav from "../Nav";
+import {Routes, Route, Navigate} from "react-router";
 
 function Labs() {
     return (
        <div>
-         <Nav/>
-         <Assignment3/>
+        <Nav/>
+        <Routes>
+          <Route path="/" element={<Navigate to="a3"/>}/>
+          <Route path="a3" element={<Assignment3/>}/>
+          <Route path="a4" element={<Assignment4/>}/>
+        </Routes>
        </div>
     );
 }
