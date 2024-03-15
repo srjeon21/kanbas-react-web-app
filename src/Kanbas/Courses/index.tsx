75b52e72c945bafd1bcb479eabad704869e4768b
Serin Serena Jeon <serin0314@gmail.com>
2024-03-15 19:16:46 -0400

    3.2 done

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -7,7 +7,7 @@ import "./index.css"
 import Home from "./Home";
 import Assignments from "./Assignments";
 
-function Courses() {
+function Courses({ courses }: { courses: any[]; }) {
   const { courseId } = useParams();
   const course = courses.find((course) => course._id === courseId);
   const { pathname } = useLocation();
