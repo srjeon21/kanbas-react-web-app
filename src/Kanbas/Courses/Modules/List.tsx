85fae9fa8b682b90c99182d4c777f52f49a6440f
Serin Serena Jeon <serin0314@gmail.com>
2024-03-16 12:11:15 -0400

    3.3.3 Module before reducer

--- src/Kanbas/Courses/Modules/List.tsx
+++ src/Kanbas/Courses/Modules/List.tsx
@@ -2,14 +2,42 @@ import React, { useState } from "react";
 import { modules } from "../../Database";
 import { FaEllipsisV, FaCheckCircle, FaPlusCircle } from "react-icons/fa";
 import { useParams } from "react-router";
+
 function ModuleList() {
   const { courseId } = useParams();
-  const modulesList = modules.filter((module) => module.course === courseId);
-  const [selectedModule, setSelectedModule] = useState(modulesList[0]);
+  const [moduleList, setModuleList] = useState<any[]>(modules);
+  const [selectedModule, setSelectedModule] = useState(moduleList[0]);
+
+  const [module, setModule] = useState({
+    name: "New Module",
+    description: "New Description",
+    course: courseId,
+  });
+  const addModule = (module: any) => {
+    const newModule = { ...module,
+      _id: new Date().getTime().toString() };
+    const newModuleList = [newModule, ...moduleList];
+    setModuleList(newModuleList);
+  };
+  const deleteModule = (moduleId: string) => {
+    const newModuleList = moduleList.filter(
+      (module) => module._id !== moduleId );
+    setModuleList(newModuleList);
+  };
+  const updateModule = (module: any) => {
+    const newModuleList = moduleList.map((m) => {
+      if (m._id === module._id) {
+        return module;
+      } else {
+        return m;
+      }
+    });
+    setModuleList(newModuleList);
+  };
+
   return (
     <>
     <div>
-
       <div className="buttons">
       <button className="btn btn-secondary">Collapse All</button>
         <button className="btn btn-secondary">View Progress</button>
@@ -18,10 +46,29 @@ function ModuleList() {
         <button className="btn btn-secondary"><FaEllipsisV/> </button>
       </div><hr/>
       <ul className="list-group wd-modules">
-        {modulesList.map((module) => (
-          <li
+      <li className="list-group-item">
+        <button onClick={() => { addModule(module) }}>
+          Add
+        </button>
+        <button onClick={updateModule}>
+                Update
+        </button>
+
+      </li>
+        {moduleList
+        .filter((module) => module.course === courseId)
+        .map((module, index) => (
+          <li key={index}
             className="list-group-item"
             onClick={() => setSelectedModule(module)}>
+              <button
+              onClick={(event) => { setModule(module); }}>
+              Edit
+            </button>
+              <button
+              onClick={() => deleteModule(module._id)}>
+              Delete
+            </button>
             <div>
               <FaEllipsisV className="me-2" />
               {module.name}
@@ -33,7 +80,7 @@ function ModuleList() {
             </div>
             {selectedModule._id === module._id && (
               <ul className="list-group">
-                {module.lessons?.map((lesson) => (
+                {module.lessons.map((lesson: { name: string }) => (
                   <li className="list-group-item">
                     <FaEllipsisV className="me-2" />
                     {lesson.name}
