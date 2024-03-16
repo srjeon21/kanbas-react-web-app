8c34388299f9d58f8754381a80c63dd7ff72a8e7
Serin Serena Jeon <serin0314@gmail.com>
2024-03-16 12:49:28 -0400

    a4 complete except styling

--- src/Kanbas/Courses/Modules/List.tsx
+++ src/Kanbas/Courses/Modules/List.tsx
@@ -2,38 +2,23 @@ import React, { useState } from "react";
 import { modules } from "../../Database";
 import { FaEllipsisV, FaCheckCircle, FaPlusCircle } from "react-icons/fa";
 import { useParams } from "react-router";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  addModule,
+  deleteModule,
+  updateModule,
+  setModule,
+} from "./reducer";
+import { KanbasState } from "../../store";
 
 function ModuleList() {
   const { courseId } = useParams();
-  const [moduleList, setModuleList] = useState<any[]>(modules);
+  const moduleList = useSelector((state: KanbasState) =>
+    state.modulesReducer.modules);
+  const module = useSelector((state: KanbasState) =>
+    state.modulesReducer.module);
+  const dispatch = useDispatch();
   const [selectedModule, setSelectedModule] = useState(moduleList[0]);
-
-  const [module, setModule] = useState({
-    name: "New Module",
-    description: "New Description",
-    course: courseId,
-  });
-  const addModule = (module: any) => {
-    const newModule = { ...module,
-      _id: new Date().getTime().toString() };
-    const newModuleList = [newModule, ...moduleList];
-    setModuleList(newModuleList);
-  };
-  const deleteModule = (moduleId: string) => {
-    const newModuleList = moduleList.filter(
-      (module) => module._id !== moduleId );
-    setModuleList(newModuleList);
-  };
-  const updateModule = (module: any) => {
-    const newModuleList = moduleList.map((m) => {
-      if (m._id === module._id) {
-        return module;
-      } else {
-        return m;
-      }
-    });
-    setModuleList(newModuleList);
-  };
 
   return (
     <>
@@ -47,26 +32,31 @@ function ModuleList() {
       </div><hr/>
       <ul className="list-group wd-modules">
       <li className="list-group-item">
-        <button onClick={() => { addModule(module) }}>
+        <button onClick={() => dispatch(addModule({ ...module, course: courseId }))}>
           Add
         </button>
-        <button onClick={updateModule}>
-                Update
+        <button onClick={() => dispatch(updateModule(module))}>
+          Update
         </button>
-
+        <input
+          value={module.name}
+          onChange={(e) =>
+            dispatch(setModule({ ...module, name: e.target.value }))
+          }/>
+        <textarea
+          value={module.description}
+          onChange={(e) =>
+            dispatch(setModule({ ...module, description: e.target.value }))
+          }/>
       </li>
         {moduleList
         .filter((module) => module.course === courseId)
         .map((module, index) => (
-          <li key={index}
-            className="list-group-item"
-            onClick={() => setSelectedModule(module)}>
-              <button
-              onClick={(event) => { setModule(module); }}>
+          <li key={index} className="list-group-item">
+              <button onClick={() => dispatch(setModule(module))}>
               Edit
             </button>
-              <button
-              onClick={() => deleteModule(module._id)}>
+              <button onClick={() => dispatch(deleteModule(module._id))}>
               Delete
             </button>
             <div>
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -4,6 +4,8 @@ import Dashboard from "./Dashboard";
 import Courses from "./Courses";
 import * as db from "./Database";
 import { useState } from "react";
+import store from "./store";
+import { Provider } from "react-redux"
 
 function Kanbas() {
   const [courses, setCourses] = useState<any[]>(db.courses);
@@ -29,33 +31,35 @@ function Kanbas() {
     );
   };
   return (
-    <div className="d-flex">
-      <div>
-        <KanbasNavigation/>
+    <Provider store={store}>
+      <div className="d-flex">
+        <div>
+          <KanbasNavigation/>
+        </div>
+        <div className="flex-fill">
+          <Routes>
+              <Route path="/" element={<Navigate to="Dashboard" />} />
+              <Route path="Account" element={<h1>Account</h1>} />
+              <Route path="Dashboard" element={<Dashboard
+                courses={courses}
+                course={course}
+                setCourse={setCourse}
+                addNewCourse={addNewCourse}
+                deleteCourse={deleteCourse}
+                updateCourse={updateCourse}/>
+            } />
+              <Route path="Courses/:courseId/*" element={<Courses courses={courses} />} />
+              <Route path="Calendar" element={<h1>Calendar</h1>} />
+              <Route path="Inbox" element={<h1>Inbox</h1>} />
+              <Route path="History" element={<h1>History</h1>} />
+              <Route path="History" element={<h1>History</h1>} />
+              <Route path="Studio" element={<h1>Studio</h1>} />
+              <Route path="Commons" element={<h1>Commons</h1>} />
+              <Route path="Help" element={<h1>Help</h1>} />
+            </Routes>
+        </div>
       </div>
-      <div className="flex-fill">
-        <Routes>
-            <Route path="/" element={<Navigate to="Dashboard" />} />
-            <Route path="Account" element={<h1>Account</h1>} />
-            <Route path="Dashboard" element={<Dashboard
-              courses={courses}
-              course={course}
-              setCourse={setCourse}
-              addNewCourse={addNewCourse}
-              deleteCourse={deleteCourse}
-              updateCourse={updateCourse}/>
-          } />
-            <Route path="Courses/:courseId/*" element={<Courses courses={courses} />} />
-            <Route path="Calendar" element={<h1>Calendar</h1>} />
-            <Route path="Inbox" element={<h1>Inbox</h1>} />
-            <Route path="History" element={<h1>History</h1>} />
-            <Route path="History" element={<h1>History</h1>} />
-            <Route path="Studio" element={<h1>Studio</h1>} />
-            <Route path="Commons" element={<h1>Commons</h1>} />
-            <Route path="Help" element={<h1>Help</h1>} />
-          </Routes>
-      </div>
-    </div>
+    </Provider>
 );}
 
 export default Kanbas;
