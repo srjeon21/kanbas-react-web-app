455361e004236910e4b7f29adc52bb585a583f60
Serin Serena Jeon <serin0314@gmail.com>
2024-03-26 01:00:02 -0400

    done with 4.1

---
+++ kanbas-node-server-app/Database/courses.js
@@ -0,0 +1,6 @@
+export default [ { "_id": "RS101", "name": "Rocket Propulsion", "number": "RS4550", "startDate": "2024-01-10",
+    "endDate": "2024-05-15", "image": "bake.jpg"  },
+  { "_id": "RS102", "name": "Aerodynamics", "number": "RS4560", "startDate": "2024-01-10",
+    "endDate": "2024-05-15", "image": "environment.jpg"  },
+  { "_id": "RS103", "name": "Spacecraft Design", "number": "RS4570", "startDate": "2024-01-10",
+    "endDate": "2024-05-15", "image": "paint.jpg"  } ]
